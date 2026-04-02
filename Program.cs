c8e3207928b429b7f932460e08936e9c5539b1e4
Mahmood Saadalla <mahmoodsadalla@gmail.com>
2026-04-02 06:18:33 +0200

    Exercise 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
+
+            #region Exercise 1
+
+            //int[] Grades = [85, 92, 78, 95, 88, 70, 100, 65];
+            //Console.WriteLine();
+            //ConsoleHelper.PrintArray("Grades", Grades);
+            //Console.WriteLine();
+
+            //Console.WriteLine($"Count of Grades = {Grades.Count()}");
+            //Console.WriteLine();
+
+            //Console.WriteLine($"First Grade = {Grades.First()}");
+            //Console.WriteLine();
+
+            //Console.WriteLine($"Last Grade = {Grades.Last()}");
+            //Console.WriteLine();
+
+            //Grades.Sort();
+            //ConsoleHelper.PrintArray("Grades After Sort", Grades);
+            //Console.WriteLine();
+            //Console.WriteLine($"First Grade Above 90 =  {Grades.FirstOrDefault(n => n > 90)}");
+            //Console.WriteLine();
             
+            //for (int i = 0; i < Grades.Length; i++)
+            //{
+            //    if (Grades[i] < 75)
+            //    {
+            //        Console.WriteLine($"Grades below 75 [failing Grades] =  {Grades[i]}");
+                    
+            //    }
+            //    ;
+                        
+            //}
+            //List<int> Grade = Grades.ToList();
+            //Grade.RemoveAll(g => g < 75);
+            //Console.WriteLine();
+
+            //Console.WriteLine($"Grades Contain 100  = {Grade.Contains(100)}");
+            //List<string> Result = new();
+            //foreach (var i in Grade)
+            //{  
+            //    Result.Add("Grade :" + i);
+            //}
+            //ConsoleHelper.PrintList("Result", Result);
+
+            #endregion
+
         }
     }
 }
