0cb6564952354a867eaccf36b197df75e50a0e1d
Mahmood Saadalla <mahmoodsadalla@gmail.com>
2026-04-02 07:14:14 +0200

    Exercise 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace G_NET_12_Adv03
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace G_NET_12_Adv03
 {
     internal class Program
     {
@@ -85,6 +87,54 @@
             //{
             //    Console.WriteLine($"Players = {item}");
             //}
+
+            #endregion
+
+            #region Exercise 3
+
+            ////Create a Collection  with 4 contacts (name → phone number)
+            ////Add a new contact using [] syntax (add or update)
+            //Dictionary<string , int> PhoneBook = new(4)
+            //{
+            //    ["Alice"] = 1234567890,
+            //    ["Bob"] = 987653210,
+            //    ["Charlie"] = 555555555,
+            //    ["Mai"] = 273462875,
+            //};
+
+            ////Try adding a duplicate using .Add() — catch the exception and print the error
+            //try
+            //{
+            //    PhoneBook.Add("Alice", 1234567890);
+
+            //}
+            //catch (Exception e) 
+            //{
+            //    Console.WriteLine(e);
+            //}
+
+            ////Try adding a duplicate using .TryAdd() — print whether it succeeded
+            //Console.WriteLine($"{PhoneBook.TryAdd("Alice", 1234567890)}"); // False
+
+            ////Search for a contact that doesn’t exist
+            //Console.WriteLine($"{PhoneBook.ContainsKey("David")}");
+
+            ////Get a contact with a fallback of "Not Found"
+            //PhoneBook.TryGetValue("David", out int number);
+            //Console.WriteLine($"{number} => Not Found");
+
+            ////Print all Keys on one line, then all Values on another line
+            //foreach (var key in PhoneBook.Keys)
+            //{
+            //    Console.Write(key + ",");
+            //}
+            //Console.WriteLine();
+            //foreach (var Value in PhoneBook.Values)
+            //{
+            //    Console.Write(Value + ",");
+            //}
+
+
 
             #endregion
 
