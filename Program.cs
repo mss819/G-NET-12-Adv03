89dba8a34cebbaee2fb4a9990923e8767d6a3fac
Mahmood Saadalla <mahmoodsadalla@gmail.com>
2026-04-02 06:36:24 +0200

    Exercise 2

--- Program.cs
+++ Program.cs
@@ -26,16 +26,16 @@
             //Console.WriteLine();
             //Console.WriteLine($"First Grade Above 90 =  {Grades.FirstOrDefault(n => n > 90)}");
             //Console.WriteLine();
-            
+
             //for (int i = 0; i < Grades.Length; i++)
             //{
             //    if (Grades[i] < 75)
             //    {
             //        Console.WriteLine($"Grades below 75 [failing Grades] =  {Grades[i]}");
-                    
+
             //    }
             //    ;
-                        
+
             //}
             //List<int> Grade = Grades.ToList();
             //Grade.RemoveAll(g => g < 75);
@@ -48,6 +48,43 @@
             //    Result.Add("Grade :" + i);
             //}
             //ConsoleHelper.PrintList("Result", Result);
+
+            #endregion
+
+            #region Exercise 2
+            ////Add: 500="Ahmed", 200="Sara", 800="Ali", 350="Mona"
+            //SortedList<int, string> Playres = new()
+            //{
+            //    [500] = "Ahmed",
+            //    [200] = "Sara",
+            //    [300] = "Ali",
+            //    [350] = "Mona",
+            //};
+            //Console.WriteLine();
+
+            ////Print all entries (they should be sorted by score automatically)
+            //foreach (var item in Playres)
+            //{
+            //    Console.WriteLine($"Players = {item}");
+            //}
+
+            ////Access the first key and first value 
+            //Console.WriteLine($"First Key  = {Playres.First().Key} , First Value  = {Playres.First().Value}");
+
+
+            ////Check if score 500 exists
+            //Console.WriteLine($"if score 500 exists ? {Playres.ContainsKey(500)}");
+            //Console.WriteLine();
+
+            ////Safely get the player with score 999
+            //Playres.ElementAtOrDefault( 999 );
+
+            ////Remove the player with score 200 and print the updated list
+            //Playres.Remove(200);
+            //foreach (var item in Playres)
+            //{
+            //    Console.WriteLine($"Players = {item}");
+            //}
 
             #endregion
 
